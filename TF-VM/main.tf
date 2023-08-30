cc662ce84110e3d8cfadf18bfa83cee2d02c26c7
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-30 16:45:59 +0000

    Updated main.tf

--- TF-VM/main.tf
+++ TF-VM/main.tf
@@ -212,15 +212,16 @@ resource "null_resource" "install_packages_jenkins" {
 provisioner "remote-exec" {
   inline = [
         "sudo apt-get update && sudo apt-get -y upgrade",
-        "sudo apt-get install -y ansible",
-        "sudo apt-get install -y apache2 wget",
+        "sudo apt-get install -y apache2 curl",
         "sudo apt-get install -y mariadb-server",
         "sudo apt-get install -y php libapache2-mod-php php-mysql",
-        "sudo apt-get install -y default-jre",
-        "sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -",
-        "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
-        "sudo apt-get update",
-        "sudo apt-get install -y jenkins"
+        "sudo apt install openjdk-8-jdk -y",
+        "sudo apt install openjdk-11-jdk -y",
+        "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
+        "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
+        "sudo apt update",
+        "sudo apt install jenkins -y",
+        "sudo ufw allow 8080"
   ]
   }
 }
