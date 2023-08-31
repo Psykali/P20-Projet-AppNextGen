653dbd753fd26b1d52114867f2a3987d38575ec5
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-31 09:36:48 +0000

    Added Network.tf

---
+++ TF-VM/Network.tf
@@ -0,0 +1,27 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+}
+
+resource "azurerm_subnet" "subnet_jenkins" {
+  name                 = "subnet-jenkins"
+  resource_group_name   = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet_admin" {
+  name                 = "subnet-admin"
+  resource_group_name   = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "lb_public_ip" {
+  name                = "lb-public-ip"
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  allocation_method  = "Static"
+}
