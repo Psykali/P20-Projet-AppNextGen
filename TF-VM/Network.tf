96177909a5379a081b44cbd940651da4c51acb4c
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-31 14:02:38 +0000

    Updated Network.tf

--- TF-VM/Network.tf
+++ TF-VM/Network.tf
@@ -11,10 +11,3 @@ resource "azurerm_subnet" "proj_subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
-resource "azurerm_public_ip" "lb_public_ip" {
-  name                = "lb-public-ip"
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  allocation_method  = "Static"
-}
