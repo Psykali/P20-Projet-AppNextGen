b3f72f050ab98981b8cc05a80725932b6e3d0117
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-31 10:18:52 +0000

    Updated VMs.tf

--- TF-VM/VMs.tf
+++ TF-VM/VMs.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_machine" "jenkins_vm" {
+resource "azurerm_linux_virtual_machine" "jenkins_vm" {
   name                = "jenkins-vm"
   location              = var.location
   resource_group_name   = var.resource_group_name
@@ -21,7 +21,7 @@ resource "azurerm_virtual_machine" "jenkins_vm" {
   }
 }
 
-resource "azurerm_virtual_machine" "admin_vm" {
+resource "azurerm_linux_virtual_machine" "admin_vm" {
   name                = "admin-vm"
   location              = var.location
   resource_group_name   = var.resource_group_name
@@ -44,4 +44,67 @@ resource "azurerm_virtual_machine" "admin_vm" {
     version   = "latest"
   }
 }
+####################
+## Bash Scripting ##
+####################
 
+resource "null_resource" "install_packages_jenkins" {
+  depends_on = [
+    azurerm_linux_virtual_machine.jenkins_vm,
+  ]
+
+  connection {
+    type     = "ssh"
+    user     = var.admin_username
+    password = var.admin_password
+    host     = azurerm_linux_virtual_machine.jenkins_vm.network_interface_ids[0]
+  }
+
+provisioner "remote-exec" {
+  inline = [
+        "sudo apt-get update && sudo apt-get -y upgrade",
+        "sudo apt-get install -y apache2 curl",
+        "sudo apt-get install -y mariadb-server",
+        "sudo apt-get install -y php libapache2-mod-php php-mysql",
+        "sudo apt install openjdk-8-jdk -y",
+        "sudo apt install openjdk-11-jdk -y",
+        "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
+        "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
+        "sudo apt update",
+        "sudo apt install jenkins -y",
+        "sudo ufw allow 8080"
+  ]
+  }
+}
+
+resource "null_resource" "install_packages_ansible" {
+  depends_on = [
+    azurerm_linux_virtual_machine.admin_vm,
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = var.admin_username
+    password = var.admin_password
+    host     = azurerm_linux_virtual_machine.admin_vm.network_interface_ids[0]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update && sudo apt-get -y upgrade",
+      "sudo apt-get install -y apache2 ca-certificates curl gnupg",
+      "sudo apt-get install -y mariadb-server",
+      "sudo apt-get install -y php libapache2-mod-php php-mysql",
+      "sudo apt-add-repository ppa:ansible/ansible",
+      "sudo apt update",
+      "sudo apt install -y ansible",
+      "sudo install -m 0755 -d /etc/apt/keyrings",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+      "sudo chmod a+r /etc/apt/keyrings/docker.gpg",
+      "echo 'deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \"$(. /etc/os-release && echo \"$VERSION_CODENAME\")\" stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt update",
+      "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y",
+    ]
+  }
+
+}
