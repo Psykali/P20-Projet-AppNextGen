25dbccb5bec0ba178b400bf4a74ee8f87181ad34
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-31 10:25:26 +0000

    Updated NATrules.tf

--- TF-VM/NATrules.tf
+++ TF-VM/NATrules.tf
@@ -1,31 +1,19 @@
 resource "azurerm_lb_nat_rule" "jenkins_nat_rule" {
-  count = 2
-
+  count                 = length(azurerm_virtual_machine.jenkins_vm)
   resource_group_name   = var.resource_group_name
-  loadbalancer_id     = azurerm_lb.lb.id
-  name                = "jenkins-nat-rule-${count.index}"
-  protocol            = "Tcp"
-  frontend_port       = 8080 + count.index
-  backend_port        = 8080
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.lb_public_ip.id
-  }
-  backend_address_pool_id = azurerm_lb_backend_address_pool.jenkins_backend_pool.id
+  loadbalancer_id       = azurerm_lb.example.id
+  protocol              = "Tcp"
+  frontend_port         = 80
+  backend_port          = 8080
+  frontend_ip_configuration_name = azurerm_lb.example.frontend_ip_configuration[0].name
 }
 
 resource "azurerm_lb_nat_rule" "admin_nat_rule" {
-  count = 2
-
+  count                 = length(azurerm_virtual_machine.admin_vm)
   resource_group_name   = var.resource_group_name
-  loadbalancer_id     = azurerm_lb.lb.id
-  name                = "admin-nat-rule-${count.index}"
-  protocol            = "Tcp"
-  frontend_port       = 8082 + count.index
-  backend_port        = 8080
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.lb_public_ip.id
-  }
-  backend_address_pool_id = azurerm_lb_backend_address_pool.admin_backend_pool.id
+  loadbalancer_id       = azurerm_lb.example.id
+  protocol              = "Tcp"
+  frontend_port         = 80
+  backend_port          = 8080
+  frontend_ip_configuration_name = azurerm_lb.example.frontend_ip_configuration[0].name
 }
