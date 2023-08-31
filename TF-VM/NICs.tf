81f3895ab75195a735850b5325efb12071ab319c
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-31 14:01:34 +0000

    Updated NICs.tf

--- TF-VM/NICs.tf
+++ TF-VM/NICs.tf
@@ -7,6 +7,17 @@ resource "azurerm_network_interface" "jenkins_nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.proj_subnet.id
     private_ip_address_allocation = "Dynamic"
+      
+      public_ip_address {
+      name                = "jenkins-public-ip"
+      location            = var.location
+      resource_group_name = var.resource_group_name
+      allocation_method   = "Static"
+      sku                 = "Basic"
+
+      dns_settings {
+        domain_name_label = "jenkins-vm"
+      }
   }
 }
 
@@ -19,6 +30,17 @@ resource "azurerm_network_interface" "admin_nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.proj_subnet.id
     private_ip_address_allocation = "Dynamic"
+      
+      public_ip_address {
+      name                = "admin-public-ip"
+      location            = var.location
+      resource_group_name = var.resource_group_name
+      allocation_method   = "Static"
+      sku                 = "Basic"
+
+      dns_settings {
+        domain_name_label = "admin-vm"
+      }
   }
 }
 
