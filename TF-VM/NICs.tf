5db58ccc92f7dee1e4f6dbe807c735f114e7babb
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-31 14:07:27 +0000

    Updated NICs.tf

--- TF-VM/NICs.tf
+++ TF-VM/NICs.tf
@@ -1,47 +1,49 @@
+resource "azurerm_public_ip" "jenkins_public_ip" {
+  name                = "jenkins-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+
+  dns_settings {
+    domain_name_label = "jenkins-vm"
+  }
+}
+
 resource "azurerm_network_interface" "jenkins_nic" {
   name                = "jenkins-nic"
-  location              = var.location
-  resource_group_name   = var.resource_group_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.proj_subnet.id
     private_ip_address_allocation = "Dynamic"
-      
-      public_ip_address {
-      name                = "jenkins-public-ip"
-      location            = var.location
-      resource_group_name = var.resource_group_name
-      allocation_method   = "Static"
-      sku                 = "Basic"
-
-      dns_settings {
-        domain_name_label = "jenkins-vm"
-      }
+    public_ip_address_id          = azurerm_public_ip.jenkins_public_ip.id
   }
 }
+
+resource "azurerm_public_ip" "admin_public_ip" {
+  name                = "admin-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Basic"
+
+  dns_settings {
+    domain_name_label = "admin-vm"
+  }
 }
 
 resource "azurerm_network_interface" "admin_nic" {
   name                = "admin-nic"
-  location              = var.location
-  resource_group_name   = var.resource_group_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.proj_subnet.id
     private_ip_address_allocation = "Dynamic"
-      
-      public_ip_address {
-      name                = "admin-public-ip"
-      location            = var.location
-      resource_group_name = var.resource_group_name
-      allocation_method   = "Static"
-      sku                 = "Basic"
-
-      dns_settings {
-        domain_name_label = "admin-vm"
-      }
+    public_ip_address_id          = azurerm_public_ip.admin_public_ip.id
   }
-}
 }
