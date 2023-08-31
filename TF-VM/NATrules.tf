25a1143d1289dfbbbbbc45cd67f19f24953a94de
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-31 10:31:10 +0000

    Updated NATrules.tf

--- TF-VM/NATrules.tf
+++ TF-VM/NATrules.tf
@@ -1,19 +1,19 @@
 resource "azurerm_lb_nat_rule" "jenkins_nat_rule" {
-  count                 = length(azurerm_virtual_machine.jenkins_vm)
+  count                 = length(azurerm_linux_virtual_machine.jenkins_vm)
   resource_group_name   = var.resource_group_name
-  loadbalancer_id       = azurerm_lb.example.id
+  loadbalancer_id       = azurerm_lb.lb.id
   protocol              = "Tcp"
   frontend_port         = 80
   backend_port          = 8080
-  frontend_ip_configuration_name = azurerm_lb.example.frontend_ip_configuration[0].name
+  frontend_ip_configuration_name = azurerm_lb.lb.frontend_ip_configuration[0].name
 }
 
 resource "azurerm_lb_nat_rule" "admin_nat_rule" {
-  count                 = length(azurerm_virtual_machine.admin_vm)
+  count                 = length(azurerm_linux_virtual_machine.admin_vm)
   resource_group_name   = var.resource_group_name
-  loadbalancer_id       = azurerm_lb.example.id
+  loadbalancer_id       = azurerm_lb.lb.id
   protocol              = "Tcp"
   frontend_port         = 80
   backend_port          = 8080
-  frontend_ip_configuration_name = azurerm_lb.example.frontend_ip_configuration[0].name
+  frontend_ip_configuration_name = azurerm_lb.lb.frontend_ip_configuration[0].name
 }
