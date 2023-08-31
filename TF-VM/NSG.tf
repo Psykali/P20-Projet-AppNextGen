aa262e603f9881f2edc65cb142c19481c5b8d5bd
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-31 12:27:26 +0000

    Updated NSG.tf

--- TF-VM/NSG.tf
+++ TF-VM/NSG.tf
@@ -1,11 +1,5 @@
-resource "azurerm_network_security_group" "nsg_jenkins" {
-  name                = "nsg-jenkins"
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-}
-
-resource "azurerm_network_security_group" "nsg_admin" {
-  name                = "nsg-admin"
+resource "azurerm_network_security_group" "proj_nsg" {
+  name                = "proj-nsg"
   location              = var.location
   resource_group_name   = var.resource_group_name
 }
@@ -21,7 +15,7 @@ resource "azurerm_network_security_rule" "nsg_rule_ssh" {
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name   = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.nsg_jenkins.name
+  network_security_group_name = azurerm_network_security_group.proj-nsg.name
 }
 
 resource "azurerm_network_security_rule" "nsg_rule_http" {
@@ -35,7 +29,7 @@ resource "azurerm_network_security_rule" "nsg_rule_http" {
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name   = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.nsg_jenkins.name
+  network_security_group_name = azurerm_network_security_group.proj-nsg.name
 }
 
 resource "azurerm_network_security_rule" "nsg_rule_https" {
@@ -49,11 +43,11 @@ resource "azurerm_network_security_rule" "nsg_rule_https" {
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name   = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.nsg_jenkins.name
+  network_security_group_name = azurerm_network_security_group.proj-nsg.name
 }
 
-resource "azurerm_network_security_rule" "nsg_rule_jenkins" {
-  name                        = "nsg-rule-jenkins"
+resource "azurerm_network_security_rule" "nsg_rule_custom" {
+  name                        = "nsg-rule-8080"
   priority                    = 1004
   direction                   = "Inbound"
   access                      = "Allow"
@@ -63,61 +57,5 @@ resource "azurerm_network_security_rule" "nsg_rule_jenkins" {
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name   = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.nsg_jenkins.name
-}
-
-resource "azurerm_network_security_rule" "nsg_rule_admin_ssh" {
-  name                        = "nsg-rule-admin-ssh"
-  priority                    = 1001
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name   = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.nsg_admin.name
-}
-
-resource "azurerm_network_security_rule" "nsg_rule_admin_http" {
-  name                        = "nsg-rule-admin-http"
-  priority                    = 1002
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "80"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name   = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.nsg_admin.name
-}
-
-resource "azurerm_network_security_rule" "nsg_rule_admin_https" {
-  name                        = "nsg-rule-admin-https"
-  priority                    = 1003
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "443"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name   = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.nsg_admin.name
-}
-
-resource "azurerm_network_security_rule" "nsg_rule_ansible" {
-  name                        = "nsg-rule-ansible"
-  priority                    = 1004
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "8080"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name   = var.resource_group_name
-  network_security_group_name = azurerm_network_security_group.nsg_admin.name
+  network_security_group_name = azurerm_network_security_group.proj-nsg.name
 }
