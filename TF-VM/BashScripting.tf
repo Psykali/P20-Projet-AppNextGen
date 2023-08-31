ac405787dfdc9656426ee2aa3c05da47fae88eda
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-08-31 09:57:54 +0000

    Updated BashScripting.tf

--- TF-VM/BashScripting.tf
+++ TF-VM/BashScripting.tf
@@ -39,21 +39,22 @@ resource "null_resource" "install_packages_ansible" {
     host     = azurerm_linux_virtual_machine.admin_vm.network_interface_ids[0]
   }
 
-provisioner "remote-exec" {
-  inline = [
-        "sudo apt-get update && sudo apt-get -y upgrade",
-        "sudo apt-get install -y apache2 ca-certificates curl gnupg",
-        "sudo apt-get install -y mariadb-server",
-        "sudo apt-get install -y php libapache2-mod-php php-mysql",
-        "sudo apt-add-repository ppa:ansible/ansible",
-        "sudo apt update",
-        "sudo apt install -y ansible",
-        "sudo install -m 0755 -d /etc/apt/keyrings",
-        "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
-        "sudo chmod a+r /etc/apt/keyrings/docker.gpg",
-        "echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-        "sudo apt update",
-        "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin"
-  ]
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update && sudo apt-get -y upgrade",
+      "sudo apt-get install -y apache2 ca-certificates curl gnupg",
+      "sudo apt-get install -y mariadb-server",
+      "sudo apt-get install -y php libapache2-mod-php php-mysql",
+      "sudo apt-add-repository ppa:ansible/ansible",
+      "sudo apt update",
+      "sudo apt install -y ansible",
+      "sudo install -m 0755 -d /etc/apt/keyrings",
+      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg",
+      "sudo chmod a+r /etc/apt/keyrings/docker.gpg",
+      "echo 'deb [arch=\"$(dpkg --print-architecture)\" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \"$(. /etc/os-release && echo \"$VERSION_CODENAME\")\" stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt update",
+      "sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y",
+    ]
   }
+
 }
