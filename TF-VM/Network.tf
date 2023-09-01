2ea4e6143851384b5fbfc7cf2a7cc1815a4a82a9
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-01 08:13:26 +0000

    Updated Network.tf

--- TF-VM/Network.tf
+++ TF-VM/Network.tf
@@ -2,7 +2,7 @@
 ## Create Vnet ##
 #################
 resource "azurerm_virtual_network" "vnet" {
-  name                = "my-vnet"
+  name                = var.virtual_network_name
   address_space       = ["10.0.0.0/16"]
   location              = var.location
   resource_group_name   = var.resource_group_name
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "vnet" {
 ## Create Subnet ##
 ###################
 resource "azurerm_subnet" "proj_subnet" {
-  name                 = "proj-subnet"
+  name                 = var.subnet
   resource_group_name   = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
