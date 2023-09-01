d81b94743fa80ef08cd76a12b6b80bf330bc6bb6
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-01 08:17:45 +0000

    Updated JenkinsVM.tf

--- TF-VM/JenkinsVM.tf
+++ TF-VM/JenkinsVM.tf
@@ -46,13 +46,13 @@ provisioner "remote-exec" {
         "sudo apt-get install -y apache2 curl",
         "sudo apt-get install -y mariadb-server",
         "sudo apt-get install -y php libapache2-mod-php php-mysql",
-        "sudo apt install openjdk-8-jdk -y",
-        "sudo apt install openjdk-11-jdk -y",
-        "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
-        "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
-        "sudo apt update",
-        "sudo apt install jenkins -y",
-        "sudo ufw allow 8080"
+#        "sudo apt install openjdk-8-jdk -y",
+#        "sudo apt install openjdk-11-jdk -y",
+#        "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
+#        "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
+#        "sudo apt update",
+#        "sudo apt install jenkins -y",
+#        "sudo ufw allow 8080"
   ]
   }
 }
