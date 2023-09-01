b616dc0c6290fe33ee29a8f9acae2c2778109259
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-01 08:05:26 +0000

    Updated Network.tf

--- TF-VM/Network.tf
+++ TF-VM/Network.tf
@@ -1,10 +1,15 @@
+#################
+## Create Vnet ##
+#################
 resource "azurerm_virtual_network" "vnet" {
   name                = "my-vnet"
   address_space       = ["10.0.0.0/16"]
   location              = var.location
   resource_group_name   = var.resource_group_name
 }
-
+###################
+## Create Subnet ##
+###################
 resource "azurerm_subnet" "proj_subnet" {
   name                 = "proj-subnet"
   resource_group_name   = var.resource_group_name
