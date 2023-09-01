6ca1c23854038c5a91bd1c70dbdb37959092e2cd
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-01 08:14:45 +0000

    Updated variables.tf

--- TF-VM/variables.tf
+++ TF-VM/variables.tf
@@ -16,7 +16,7 @@ variable "admin_username" {
 }
 
 variable "admin_password" {
-  default = "P@ssw0rd123P@ssw0rd123"
+  default = "gqtErHR7Khdz!3B#M@waw"
 }
 ########
 ## VM ##
@@ -38,22 +38,6 @@ variable "virtual_network_name" {
   default = "skwpp20vnet"
 }
 
-variable "virtual_network" {
-  default = "skwpp20net"
-}
-
-variable "network_interface" {
-  default = "skwpp20nic"
-}
-
 variable "network_security_group_name" {
   default = "skwpp20nsg"
-}
-
-variable "ubuntu-ipconfig" {
-  default = "skwpp20ip"
-}
-
-variable "ubuntu-pip" {
-  default = "skwpp20pip"
 }
