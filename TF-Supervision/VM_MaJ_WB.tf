362f2c1d1cdf15e42e23945a10a8f4d75cb86a4c
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-06 17:27:02 +0000

    Updated VM_MaJ_WB.tf

--- TF-Supervision/VM_MaJ_WB.tf
+++ TF-Supervision/VM_MaJ_WB.tf
@@ -2,7 +2,7 @@
 ## Deploy MàJ Reporting WorkBook ##
 ###################################
 resource "azurerm_application_insights_workbook" "VM_MAJ_Reporting" {
-  name                = uuidv4()
+  name                = "vmpatching"
   location            = var.location
   resource_group_name = var.resource_group_name
   display_name        = "VM_MAJ_Reporting"
