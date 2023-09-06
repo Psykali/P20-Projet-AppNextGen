65e07cb7956dc76bc8324c609089cc46a2b5cf7a
Siefeleslam Khalifa <skhalifa@simplonformations.onmicrosoft.com>
2023-09-06 14:36:12 +0000

    Updated Provider.tf

--- TF-VM/Provider.tf
+++ TF-VM/Provider.tf
@@ -15,7 +15,7 @@ terraform {
     resource_group_name  = "PERSO_SIEF"
     storage_account_name = "sppersotfstates"
     container_name       = "sakvtfstate"
-    key                  = "jenkinsaks.tfstate"
+    key                  = "jenkinsaksvm.tfstate"
   }
 }
 
